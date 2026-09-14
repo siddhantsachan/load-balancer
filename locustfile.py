50ab99c89d95948ab8fd6576954ca26156db62da
siddhantsachan <siddhantsachan18@gmail.com>
2026-09-15 05:00:00 +0530

    add locust load testing

---
+++ locustfile.py
@@ -0,0 +1,8 @@
+from locust import HttpUser, task, between
+
+class LoadBalancerUser(HttpUser):
+    wait_time = between(0.1, 0.5)
+
+    @task
+    def hit_proxy(self):
+        self.client.get("/")
